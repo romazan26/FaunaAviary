988647fca7fc69e0368f6f5f274ed97169846494
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-07-16 13:24:39 +0300

    start settings project

---
+++ FaunaAviary/View/CreateTitleZooView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTitleZooView.swift
+//  FaunaAviary
+//
+//  Created by Роман on 16.07.2024.
+//
+
+import SwiftUI
+
+struct CreateTitleZooView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTitleZooView()
+}
